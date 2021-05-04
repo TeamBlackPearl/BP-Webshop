00cf96d161ab62d977ebf0ecf09efdc88d80b967
ebru0152 <74714355+ebru0152@users.noreply.github.com>
2021-05-04 11:37:09 +0200

    Most Model classes

---
+++ Models/AdminUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BP_Webshop.Models
+{
+    public class AdminUser : AUser
+    {
+        public string Username { get; set; }
+
+        public AdminUser()
+        {
+            
+        }
+
+        public AdminUser(int id, string firstName, string lastName, string username, string password) : base(id, firstName, lastName, password)
+        {
+            Username = username;
+        }
+    }
+}
---
+++ Models/OrderLine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BP_Webshop.Models
+{
+    public class OrderLine
+    {
+        public Order OrderId { get; set; }
+        public Jewelry JewelryId { get; set; }
+        public int ProductCount { get; set; }
+
+        public OrderLine()
+        {
+            
+        }
+        public OrderLine(Order orderId, Jewelry jewelryId, int productCount)
+        {
+            OrderId = orderId;
+            JewelryId = jewelryId;
+            ProductCount = productCount;
+        }
+    }
+}
