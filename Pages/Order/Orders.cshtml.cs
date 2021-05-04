7603f2f087c690dea2a23e003c8a971f7474755c
Mai Lionett Nielsen <maix2374@edu.easj.dk>
2021-05-04 12:51:07 +0200

    Orders - den er ikke færdig! Og userservice mappen er oprettet

---
+++ Pages/Order/Orders.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BP_Webshop.Models;
+using BP_Webshop.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BP_Webshop.Pages.Order
+{
+    public class OrdersModel : PageModel
+    {
+        public UserService UserService { get; set;  }
+
+        //IEnumrable
+        public IEnumerable<Models.Order> Orders { get; set; }
+
+        public OrdersModel(UserService userService)
+        {
+            UserService = userService;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BP_Webshop.Pages.Order
+namespace BP_Webshop.Services
 {
-    public class MyOrder
+    public class UserService
     {
     }
 }
