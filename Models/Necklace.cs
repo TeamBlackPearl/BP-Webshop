d17caa435fb64581b450c260675ba356913d2ecc
ebru0152 <74714355+ebru0152@users.noreply.github.com>
2021-05-04 10:44:23 +0200

    Jewelry and categories added

---
+++ Models/Necklace.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BP_Webshop.Models
+{
+    public class Necklace : Jewelry
+    {
+        public enum NecklaceTypes
+        {
+
+        }
+
+        public double NecklaceLength { get; set; }
+        public double NecklaceWidth { get; set; }
+
+        public Necklace()
+        {
+
+        }
+
+        public Necklace(int jewelryId, string jewelryTitle, string description, string color, decimal price, double necklaceLength, double necklaceWidth, double averageRating, string imageLink) : base(jewelryId, jewelryTitle, description, color, price, averageRating, imageLink)
+        {
+            NecklaceLength = necklaceLength;
+            NecklaceWidth = necklaceWidth;
+        }
+
+    }
+}
