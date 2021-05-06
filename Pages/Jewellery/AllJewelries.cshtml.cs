d7d0703f6825d93010b04b0fb6103ef7c42764b3
ebru0152 <74714355+ebru0152@users.noreply.github.com>
2021-05-06 12:34:10 +0200

    Search funktion i All Jewelry siden Create, delete and edit virker i necklace og bracelet

---
+++ Pages/Jewellery/AllJewelries.cshtml.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BP_Webshop.Models;
+using BP_Webshop.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BP_Webshop.Pages.Jewellery
+{
+    public class AllJewelriesModel : PageModel
+    {
+        private JewelryService jewelryService;
+        public List<Jewelry> Jewelries { get; set; }
+
+        [BindProperty]
+        public Jewelry Jewelry { get; set; }
+
+
+        public AllJewelriesModel(JewelryService jewelryService)
+        {
+            this.jewelryService = jewelryService;
+        }
+
+
+        public IActionResult OnGet()
+        {
+            Jewelries = jewelryService.GetAllJewelries().ToList();
+            return Page();
+
+
+        }
+
+        public IActionResult OnPost(string searchCriteria)
+        {
+            searchCriteria ??= "";
+
+            if (jewelryService != null)
+            {
+                Jewelries = jewelryService.GetAllJewelries()
+                    .FindAll(x => x.JewelryTitle.ToUpper().Contains(searchCriteria.ToUpper()));
+            }
+
+            return Page();
+        }
+    }
+}
---
+++ Services/JewelryService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BP_Webshop.Models;
+
+namespace BP_Webshop.Services
+{
+    public class JewelryService
+    {
+        public List<Jewelry> Jewelries { get; set; }
+        private BraceletService braceletService;
+        private NecklaceService necklaceService;
+        private EarringService earringService;
+
+        public JewelryService(BraceletService braceletService, NecklaceService necklaceService, EarringService earringService)
+        {
+            this.braceletService = braceletService;
+            this.necklaceService = necklaceService;
+            this.earringService = earringService;
+        }
+
+        public List<Jewelry> GetAllJewelries()
+        {
+            Jewelries = new List<Jewelry>(necklaceService.GetNecklaces().Count() +
+                                          braceletService.GetBracelets().Count() +
+                                          earringService.GetEarrings().Count());
+            Jewelries.AddRange(necklaceService.GetNecklaces());
+            Jewelries.AddRange(braceletService.GetBracelets());
+            Jewelries.AddRange(earringService.GetEarrings());
+            return Jewelries;
+        }
+
+        public IEnumerable<Jewelry> SearchInJewelries(string criteria)
+        {
+            List<Jewelry> searchResults = new List<Jewelry>();
+            if (criteria != null)
+            {
+                foreach (var jewelry in Jewelries)
+                {
+                    if (jewelry.JewelryTitle.ToLower().Contains(criteria.ToLower()))
+                    {
+                        searchResults.Add(jewelry);
+                    }
+                }   
+            }
+
+            return searchResults;
+        }
+
+
+
+    }
+}
