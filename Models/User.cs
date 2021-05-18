11bd456bbdc74c4c01cb9b7e170672ffe31d8304
Mai Lionett Nielsen <maix2374@edu.easj.dk>
2021-05-18 23:45:25 +0200

    HTML forme, admin role, payment htmlsite, logo mm.

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace BP_Webshop.Models
 {
     public class User : AUser
     {
-        //optional
+        //[Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
         public string Address { get; set; }
         [Required(ErrorMessage = "you need to enter your phone number"), Range(8,8, ErrorMessage = "Has to be 8 digits")]
         public string PhoneNumber { get; set; }
---
+++ Services/OrderLineService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BP_Webshop.Models;
+
+namespace BP_Webshop.Services
+{
+    public class OrderLineService
+    {
+        public List<OrderLine> OrderLineList { get; set; }
+
+        public GenericCRUDMethods<OrderLine> DbService { get; set; }
+
+        public OrderLineService(GenericCRUDMethods<OrderLine> dbService)
+        {
+            DbService = dbService;
+            OrderLineList = DbService.GetObjectsAsync().Result.ToList();
+
+        }
+
+        public IEnumerable<OrderLine> GetOrderLines()
+        {
+            return OrderLineList;
+        }
+
+
+
+    }
+}
+
