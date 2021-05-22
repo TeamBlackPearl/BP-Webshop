a72b023d5cdef6096a5472099177ef00260ddaab
Mai Lionett Nielsen <maix2374@edu.easj.dk>
2021-05-22 15:12:09 +0200

    db_updated_search_works

---
+++ Migrations/20210522130753_BlDatabase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BP_Webshop.Migrations
+{
+    public partial class BlDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "BraceletType",
+                table: "Jewelries",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "BraceletType",
+                table: "Jewelries",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
